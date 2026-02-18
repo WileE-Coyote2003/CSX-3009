67b10b150f6efd1fd66b9cd203163a863833cda2
Thwin Htoo Aung <htoohtoo.mdy.mmr@gmail.com>
2026-02-18 19:38:03 +0700

    18ffeb

--- week12/KnapsackBound.py
+++ week12/KnapsackBound.py
@@ -1,27 +1,81 @@
+class obj:
+    def __init__(self, w, v):
+        self.w = w
+        self.v = v
+        self.r = v / w
 
-'''
-# item must be sorted in decreasing order of r
 
+x = input().split()
+N = int(x[0])
+M = int(x[1])
+
+w = input().split()
+v = input().split()
+
+item = []
+for i in range(N):
+    item.append(obj(int(w[i]), int(v[i])))
+
+# Required for correct bounding
 def getKey(x):
     return x.r
 
 item.sort(key=getKey, reverse=True)
 
-'''
-	
-def Bound(i, C):	# object i -> n-1, capacity = C
+
+def Bound(i, C):
     global item, N
     
     sw = 0
     sv = 0
     j = i
     f = 1.0
+    
     while j < N and f == 1.0:
-        wj = min(C-sw, item[j].w)
-        f = float(wj)/item[j].w
-        sw += f*item[j].w
-        sv += f*item[j].v
+        wj = min(C - sw, item[j].w)
+        f = float(wj) / item[j].w
+        sw += f * item[j].w
+        sv += f * item[j].v
         j += 1
+    
     return sv
 
 
+maxV = 0
+count = 0
+
+def dfs(i, sumW, sumV):
+    global maxV, count, N, M, item
+    
+    count += 1
+    
+    # Prune overweight
+    if sumW > M:
+        return
+    
+    # Bounding prune
+    if sumV + Bound(i, M - sumW) <= maxV:
+        return
+    
+    if i == N:
+        maxV = max(maxV, sumV)
+    else:
+        # Compute bound for both branches
+        skipBound = sumV + Bound(i+1, M - sumW)
+        takeBound = -1
+        
+        if sumW + item[i].w <= M:
+            takeBound = sumV + item[i].v + Bound(i+1, M - (sumW + item[i].w))
+        
+        # Explore more promising branch first
+        if takeBound > skipBound:
+            dfs(i+1, sumW + item[i].w, sumV + item[i].v)
+            dfs(i+1, sumW, sumV)
+        else:
+            dfs(i+1, sumW, sumV)
+            dfs(i+1, sumW + item[i].w, sumV + item[i].v)
+
+dfs(0, 0, 0)
+
+print(maxV)
+print(count)
--- week12/dfsKnapsack.py
+++ week12/dfsKnapsack.py
@@ -1,35 +1,35 @@
-
 class obj:
-    def __init__(self,w,v):
+    def __init__(self, w, v):
         self.w = w
         self.v = v
-        self.r = v/w
+        self.r = v / w
+
 
 x = input().split()
 N = int(x[0])
 M = int(x[1])
+
 w = input().split()
 v = input().split()
+
 item = []
 for i in range(N):
-    item.append(obj(int(w[i]),int(v[i])))
+    item.append(obj(int(w[i]), int(v[i])))
 
 maxV = 0
 
 def dfs(i, sumW, sumV):
     global maxV, item, N, M
-    
+
+    # Step 5 pruning: overweight -> stop
+    if sumW > M:
+        return
+
     if i == N:
-        if sumW <= M:
-            maxV = max(maxV, sumV)
+        maxV = max(maxV, sumV)
     else:
-        dfs(i+1, sumW, sumV)
-        dfs(i+1, sumW+item[i].w, sumV+item[i].v)
+        dfs(i + 1, sumW, sumV)
+        dfs(i + 1, sumW + item[i].w, sumV + item[i].v)
 
-dfs(0,0,0)
+dfs(0, 0, 0)
 print(maxV)
-
-
-
-
-    
